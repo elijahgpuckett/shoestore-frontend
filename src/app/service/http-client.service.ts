da30857a4176bce578e614dba8737ce683dc8666
elijahgpuckett <elijahgabrielp@gmail.com>
2020-08-19 14:22:10 -0400

    converted linkg to heroku

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -12,37 +12,37 @@ export class HttpClientService {
   }
 
   getUsers() {
-    return this.httpClient.get<User[]>('http://localhost:8080/users/get');
+    return this.httpClient.get<User[]>('https://shoestore-backend.herokuapp.com/users/get');
   }
 
   addUser(newUser: User) {
-    return this.httpClient.post<User>('http://localhost:8080/users/add', newUser);   
+    return this.httpClient.post<User>('https://shoestore-backend.herokuapp.com/users/add', newUser);   
   }
 
   deleteUser(id) {
-    return this.httpClient.delete<User>('http://localhost:8080/users/' + id);
+    return this.httpClient.delete<User>('https://shoestore-backend.herokuapp.com/users/' + id);
   }
 
   getShoes() {
-    return this.httpClient.get<Shoe[]>('http://localhost:8080/shoes/get');
+    return this.httpClient.get<Shoe[]>('https://shoestore-backend.herokuapp.com/shoes/get');
   }
 
   addUploadData(selectedFile) {
 
-    return this.httpClient.post('http://locahost:8080/shoes/upload', selectedFile);
+    return this.httpClient.post('https://shoestore-backend.herokuapp.com/shoes/upload', selectedFile);
   }
 
   addShoe(newShoe: Shoe) {
     console.log("add shoe")
-    return this.httpClient.post<Shoe>('http://localhost:8080/shoes/add', newShoe);
+    return this.httpClient.post<Shoe>('https://shoestore-backend.herokuapp.com/shoes/add', newShoe);
   }
 
   deleteShoe(id) {
-    return this.httpClient.delete<Shoe>('http://localhost:8080/shoes/' + id);
+    return this.httpClient.delete<Shoe>('https://shoestore-backend.herokuapp.com/shoes/' + id);
   }
 
   updateShoe(updatedShoe: Shoe) {
-    return this.httpClient.put<Shoe>('http://localhost:8080/shoes/update', updatedShoe);
+    return this.httpClient.put<Shoe>('https://shoestore-backend.herokuapp.com/shoes/update', updatedShoe);
   }
   
 }
