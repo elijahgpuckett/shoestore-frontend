fe36a7ee5f440329e657723702e0c3732cbf5851
elijahgpuckett <elijahgabrielp@gmail.com>
2020-08-19 13:39:28 -0400

    third update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shoe-store'));
+app.use(express.static(__dirname + '/dist/ShoeStore'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/shoe-store/index.html'));});
+'/dist/ShoeStore/index.html'));});
 app.listen(process.env.PORT || 8080);
